af90248b45e92c5688d85d40e70e6c96a750830f
Agustin <agustinmarinlautaro1001@gmail.com>
2025-05-30 17:51:56 -0300

    ParteAgustin

--- src/model/Persona.java
+++ src/model/Persona.java
@@ -1,5 +1,49 @@
 package model;
 
 public class Persona {
+    private int id;
+    private String nombre;
+    private int edad;
+    private Domicilio domicilio;
 
+    public Persona(int id, String nombre, int edad, Domicilio domicilio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.domicilio = domicilio;
+    }
+
+    public Persona(String nombre, int edad, Domicilio domicilio) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.domicilio = domicilio;
+    }
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public int getEdad() { return edad; }
+    public void setEdad(int edad) { this.edad = edad; }
+
+    public Domicilio getDomicilio() { return domicilio; }
+    public void setDomicilio(Domicilio domicilio) { this.domicilio = domicilio; }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", domicilio=" + domicilio +
+                '}';
+    }
 }
